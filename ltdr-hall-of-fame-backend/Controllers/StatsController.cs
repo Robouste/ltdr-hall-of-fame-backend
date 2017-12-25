81e1e09c5953e8a77ab2e64a5625d3cfcb50c118
Robouste <robindenis88@gmail.com>
2017-12-25 01:34:47 +0100

    - stats fix

--- ltdr-hall-of-fame-backend/Controllers/StatsController.cs
+++ ltdr-hall-of-fame-backend/Controllers/StatsController.cs
@@ -28,18 +28,18 @@ namespace ltdr_hall_of_fame_backend.Controllers
             var jokesContext = _context.Jokes.Include(joke => joke.Votes).ToList();
             var jokes = AutoMapper.Mapper.Map<List<JokeViewModel>>(jokesContext);
 
-            var bestJoke = jokes.Aggregate((j1, j2) => j1.UpVotes > j2.DownVotes ? j1 : j2);
+            var bestJoke = jokes.Aggregate((j1, j2) => j1.UpVotes > j2.UpVotes ? j1 : j2);
             var worstJoke = jokes.Aggregate((j1, j2) => j1.DownVotes > j2.DownVotes ? j1 : j2);
 
             var bestAuthor = jokes.Where(joke => joke.UpVotes > joke.DownVotes)
                                 .GroupBy(j => j.Author)
                                 .OrderByDescending(grp => grp.Count())
-                                .First().Key;
+                                .FirstOrDefault()?.Key;
 
             var worstAuthor = jokes.Where(joke => joke.DownVotes > joke.UpVotes)
                                 .GroupBy(j => j.Author)
                                 .OrderByDescending(grp => grp.Count())
-                                .First().Key;
+                                .FirstOrDefault()?.Key;
 
             StatsViewModel result = new StatsViewModel()
             {
